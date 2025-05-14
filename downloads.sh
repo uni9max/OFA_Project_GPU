81761c968fae8550201eab37ca009fd27f9fd6fb
Maksimas Zenkevicius <maks.baziuk@gmail.com>
2025-05-14 19:05:42 +0200

    Update downloads.sh with correct Drive links

--- downloads.sh
+++ downloads.sh
@@ -3,15 +3,15 @@
 echo "Starting download of large files..."
 
 # CHECKPOINT
-gdown --id 1ABCdefGHIJcaptionModelID -O checkpoints/caption/caption_large_best_clean.pt
+gdown --fuzzy "https://drive.google.com/file/d/1NQEzVWgSR_el4MsK-wuUb8o044iKhvij/view?usp=drive_link"
 
 # DATASETS
-gdown --id 2XYZabcSTAGE1tsvID -O dataset/caption_data/caption_stage1_train.tsv
-gdown --id 3LMNopqSTAGE2tsvID -O dataset/caption_data/caption_stage2_train.tsv
-gdown --id 4JKLcapTESTtsvID -O dataset/caption_data/caption_test.tsv
-gdown --id 5MNOcapVALtsvID -O dataset/caption_data/caption_val.tsv
+gdown --fuzzy "https://drive.google.com/file/d/1Dxbk-CMPrbB4BztEsYrTpxQDwLsg5HOr/view?usp=drive_link"
+gdown --fuzzy "https://drive.google.com/file/d/10SB1lQ9ENbrPaETCjzY6DJPm9GjYGNfS/view?usp=drive_link"
+gdown --fuzzy "https://drive.google.com/file/d/1JRtE-Ghi2Syu0RL-pcCKywcU8bBiResp/view?usp=drive_link"
+gdown --fuzzy "https://drive.google.com/file/d/1kyQTsNk8QsFGB2QF_oIj4C75j3IUQ6ag/view?usp=drive_link"
 
 # CACHED TOKEN
-gdown --id 6PQRcocoTOKID -O dataset/caption_data/cider_cached_tokens/coco-train-words.p
+gdown --fuzzy "https://drive.google.com/file/d/1DiHNM8MaqJnrQ6OrXLfuSnoTLpP0pEC1/view?usp=drive_link"
 
 echo "✅ All files downloaded successfully!"
